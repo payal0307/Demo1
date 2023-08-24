7538ee4323477558ef47b453e0d40c5e8e4f4e88
payal0307 <123103563+payal0307@users.noreply.github.com>
2023-08-24 18:00:32 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,108 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
 using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace crudOperation
+namespace datasetExample
 {
     class Program
     {
         static void Main(string[] args)
         {
+            SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["StockConnection"].ToString());
 
-            Program p = new Program();
-            //p.insertrecordThroughSP();
-           // p.updaterecordThrough();
-            p.deleterecordThrough();
+            SqlDataAdapter data = new SqlDataAdapter("select * from Stocks; select * from PriceHistory; select * from Users; select * from Orders;select * from Trade;", sqlcon);
+            DataSet ds = new DataSet();
+            data.Fill(ds);
+           
+            ds.Tables[0].TableName = "Stocks";
+            ds.Tables[1].TableName = "PriceHistory";
+            ds.Tables[2].TableName = "Users";
+            ds.Tables[3].TableName = "Orders";
+            ds.Tables[4].TableName = "Trade";
+            
+
+            foreach (DataRow row in ds.Tables["Stocks"].Rows)
+            {
+                Console.WriteLine(row["stockId"] + ", " + row["stockSymbol"] + ",  " + row["stockName"]);
+            }
+            Console.WriteLine();
+            foreach (DataRow row in ds.Tables["PriceHistory"].Rows)
+            {
+                Console.WriteLine(row["priceId"] + "," + row["stockId"] + ", " + row["openPrice"] + " ," +row["highPrice"] +" ," + row["lowPrice"] +"," +row["closePrice"]);
+            }
+            Console.WriteLine();
+            foreach (DataRow row in ds.Tables["Users"].Rows)
+            {
+                Console.WriteLine(row["userId"] + ", " + row["userName"] + ",  " + row["email"]);
+            }
+            Console.WriteLine();
+            foreach (DataRow row in ds.Tables["Orders"].Rows)
+            {
+                Console.WriteLine(row["orderId"] + ", " + row["stockId"] + ", " + row["userId"] + "," + row["orderType"] + " ," + row["orderPrice"] + "," + row["orderQuantity"]);
+            }
+
+            Console.WriteLine();
+            foreach (DataRow row in ds.Tables["Trade"].Rows)
+            {
+                Console.WriteLine(row["tradeId"] + ", " + row["stockId"] + "," + row["userId"] + " ," + row["tradeType"] + " ," + row["tradePrice"] + "," + row["tradeQuantity"]);
+            }
+            Console.WriteLine();
             Console.ReadKey();
-        }
-
-        public void insertrecordThroughSP()
-        {
-
-            SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["StockConnection"].ToString());
-
-
-            string query = "insertdata";
-            SqlCommand cmd = new SqlCommand(query, sqlcon);
-            cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            cmd.Parameters.Add("@stockId", SqlDbType.Int).Value = 0;
-            cmd.Parameters.Add("@stockSymbol", SqlDbType.VarChar).Value = "APPL";
-            cmd.Parameters.Add("@stockName", SqlDbType.VarChar).Value = "Apple inc.";
-            cmd.Parameters.Add("@createdOn", SqlDbType.DateTime).Value = DateTime.Now;
-
-            sqlcon.Open();
-            int rowaffected = cmd.ExecuteNonQuery();
-            sqlcon.Close();
-            
-        }
-        public void updaterecordThrough()
-        {
-            SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["StockConnection"].ToString());
-
-            //string query = "update Stocks set stockSymbol='FB',stockName='Facebook inc.' where stockId=3";
-
-            string updatequery = "updatedata";
-            SqlCommand cmd = new SqlCommand(updatequery, sqlcon);
-
-            cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            cmd.Parameters.Add("@stockId", SqlDbType.Int).Value = 0;
-            cmd.Parameters.Add("@stockSymbol", SqlDbType.VarChar).Value = "fb";
-            cmd.Parameters.Add("@stockName", SqlDbType.VarChar).Value ="facebook" ;
-            cmd.Parameters.Add("@createdOn", SqlDbType.DateTime).Value = DateTime.Now;
-
-            sqlcon.Open();
-            int rowaffected = cmd.ExecuteNonQuery();
-            sqlcon.Close();
-             
-        }
-        public void deleterecordThrough()
-        {
-            SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["StockConnection"].ToString());
-
-            //string query = "delete from Stocks where stockId=10";
-
-            string deletequery = "deletedata";
-            SqlCommand cmd = new SqlCommand(deletequery, sqlcon);
-
-            cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            cmd.Parameters.Add("@stockId", SqlDbType.Int).Value = 10;
-
-            
-
-            sqlcon.Open();
-            int rowaffected = cmd.ExecuteNonQuery();
-            sqlcon.Close();
-          }
-
-        public void Datashow()
-        {
-            SqlConnection sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["StockConnection"].ToString());
-
-            string query = "select stockSymbol,stockName from Stocks";
-            SqlCommand cmd = new SqlCommand(query, sqlcon);
-            sqlcon.Open();
-            SqlDataReader read = cmd.ExecuteReader();
-            
-            
-                while (read.Read())
-                {
-                    Console.WriteLine("{0} {1}", read.GetString(0), read.GetString(1));
-
-                }
-            
-            read.Close();
-            Console.ReadKey();
-
-
+           
 
 
         }
 
     }
-   
 }
