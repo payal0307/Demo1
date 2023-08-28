30f12e7d8e7ef1fb1d7f6c16cde4f6c8fb48990b
payal0307 <123103563+payal0307@users.noreply.github.com>
2023-08-28 11:48:51 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -44,4 +44,46 @@ INSERT INTO Exchange VALUES (1, 'AAPL', 'Apple Inc.',  157.50, 120000, 'NSE',get
 INSERT INTO Exchange VALUES (2, 'MSFT', 'Microsoft Corporation', 310.25, 85000, 'BSE',getdate())
     (3, 'RELIANCE', 'Reliance Industries Ltd.', 2500.75, 50000, 'NSE'),
     (4, 'TCS', 'Tata Consultancy Services Ltd.', 3500.25, 60000, 'BSE');
-	
+
+
+Introduction
+The Stock Market System is a software application that provides a platform for investors to buy and sell stocks. It aims to facilitate efficient trading, provide real-time market data, and enable investors to make informed decisions. This document serves as the Software Requirements Specification (SRS) for the Stock Market System.
+
+2. Functional Requirements
+2.1 User Registration
+   - The system shall allow users to register by providing their personal information.
+   - The system shall validate user information and ensure unique usernames.
+
+2.2 User Authentication
+   - The system shall provide a login mechanism for registered users.
+   - The system shall authenticate users based on their login credentials.
+
+2.3 Stock Listings
+   - The system shall provide a list of available stocks with their current prices.
+   - The system shall display detailed information about each stock, including historical data and company information.
+
+2.4 Buying/Selling Stocks
+   - The system shall allow users to place buy or sell orders for stocks.
+   - The system shall validate the availability of stocks and user account balances before executing orders.
+   - The system shall update user account balances and stock quantities based on successful transactions.
+
+2.5 Portfolio Management
+   - The system shall maintain a portfolio for each user, displaying their current holdings and their market value.
+   - The system shall provide a summary of the user's portfolio performance, including profit/loss calculations.
+
+2.6 Real-time Market Data
+   - The system shall provide real-time stock market data, including prices, volumes, and trends.
+   - The system shall display real-time updates to users, ensuring accurate and timely information.
+
+2.7 Transaction History
+   - The system shall maintain a transaction history for each user, recording all buy/sell orders and their outcomes.
+   - The system shall allow users to view their transaction history, including details such as date, time, stock, quantity, and price.
+
+3. Non-Functional Requirements
+3.1 Performance
+   - The system shall handle a large number of concurrent users without significant performance degradation.
+   - The system shall provide real-time updates with minimal latency.
+
+3.2 Security
+   - The system shall ensure secure transmission of user information and financial data.
+   - The system shall implement appropriate authentication and authorization mechanisms to protect user accounts.
